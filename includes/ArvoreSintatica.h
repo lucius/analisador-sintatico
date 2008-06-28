078d5c5f992f97e0d4501e6d8f3e9ebb95be3cf1
vibortolotti <vibortolotti@0e1408be-bd50-0410-9c67-7da989965b07>
2008-06-28 21:50:21 +0000

    < Funcao Hash concluida > < TabelaHash definida (falta apenas definir a struct de atributos) > < NoArvoreSintatica conclúido > < ArvoreSintatica iniciada >

---
+++ includes/ArvoreSintatica.h
@@ -0,0 +1,28 @@
+#ifndef ARVORESINTATICA_H_
+#define ARVORESINTATICA_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../includes/NoArvoreSintatica.h"
+
+
+
+class ArvoreSintatica
+{
+	public:
+		ArvoreSintatica( );
+
+		virtual
+		~ArvoreSintatica( );
+
+	protected:
+
+	private:
+		NoArvoreSintatica*
+		programa;
+
+};
+
+#endif /* ARVORESINTATICA_H_ */
